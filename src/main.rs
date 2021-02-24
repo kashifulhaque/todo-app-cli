da651343906892abd4c46db60abf9eff235a39ea
kashifulhaque <haque.kashiful7@gmail.com>
2021-02-24 19:46:56 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap
+
+fn main() {
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
+}
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push(&record)
+        }
+
+        std::fs::write("db.txt", content)
+    }
+}
