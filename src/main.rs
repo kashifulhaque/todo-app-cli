9e8f4525fc8d0c8ded7c95f2e94bb20bb97df210
kashifulhaque <haque.kashiful7@gmail.com>
2021-02-24 19:50:49 +0530

    First prototype

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::collections::HashMap
+use std::collections::HashMap;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
-
     println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("TODO saved!"),
+            Err(e) => println!("An error occurred: {}", e),
+        }
+    }
 }
 
 struct Todo {
@@ -20,7 +31,7 @@ impl Todo {
         let mut content = String::new();
         for (k, v) in self.map {
             let record = format!("{}\t{}\n", k, v);
-            content.push(&record)
+            content.push_str(&record)
         }
 
         std::fs::write("db.txt", content)
